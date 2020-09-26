cf329fbf28b9d705017d8eae98404188b14539d4
saiimmadi <meetnarasimhaimmadigmail.com>
2020-09-26 17:59:00 +0530

    Added minor changes

--- Selenium-jenkins-Git/src/test/java/com/crm/qa/tests/LoginPageTest.java
+++ Selenium-jenkins-Git/src/test/java/com/crm/qa/tests/LoginPageTest.java
@@ -1,6 +1,7 @@
 package com.crm.qa.tests;
 
 import org.testng.Assert;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -8,6 +9,8 @@ import org.testng.annotations.Test;
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.HomePage;
 import com.crm.qa.pages.LoginPage;
+
+// Author Sai Immadi
 
 public class LoginPageTest extends TestBase{
 	LoginPage loginPage;
@@ -32,7 +35,7 @@ public class LoginPageTest extends TestBase{
 	
 	
 	
-	@AfterMethod
+	@AfterMethod 
 	public void tearDown(){
 		driver.quit();
 	}
